07d8c7b4fa1e20378a4432e8be4916ad8321e5e6
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-10-07 18:23:48 -0600

    trying to get post route for email set up

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -2,6 +2,11 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
+var keys = require("../keys.js");
+var api_key = keys.keys.secret;
+var domain = 'sandbox5e5714634bda4e73801a54418c3a1b59.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
 var apiRoutes = function(app){
 	//homepage
 	app.get("/", function(req, res) {
@@ -26,6 +31,22 @@ var apiRoutes = function(app){
 		// res.send("contact request")
 	    res.sendFile(path.join(__dirname, "..", "public", "contact.html"));
 	});
+
+	app.post('/api/contact', function(req, res) {
+		console.log(req.body);
+
+		var data = {
+			from: req.body.email,
+			to: 'sjdiazrivera@gmail.com',
+			subject: req.body.subject,
+			text: req.body.message
+		};
+
+		mailgun.messages().send(data, function (error, body) {
+  			console.log(body);
+		});
+	});
+
 }
 
 module.exports = apiRoutes;
