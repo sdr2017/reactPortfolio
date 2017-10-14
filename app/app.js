3fa33cf635e15790b5bfea20b789de5f0e4cffb6
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-10-13 23:42:19 -0600

    making progress on layout, getting carousel to work

--- app/app.js
+++ app/app.js
@@ -11,3 +11,36 @@ ReactDOM.render(
 	<Hello />,
 	document.getElementById('app')
 );
+
+const Copyright = () => {
+	return (
+		<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        	<p>Copyright 2017 Sarah Diaz-Rivera</p> 
+        </div>
+	)
+}
+
+ReactDOM.render(
+	<Copyright />,
+	document.getElementById('copyright')
+);
+
+const Icons = () => {
+
+	return (
+		<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        	<img
+            alt="instagram"
+            src="assets/images/instagram.png"
+            width="100%"
+          />
+        </div>
+    )
+}
+
+ReactDOM.render(
+	<Icons />,
+	document.getElementById('icons')
+);
+
+
