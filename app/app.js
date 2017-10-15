9632f62e6a1be8b84201173a09bebbf0d55de8fa
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-10-14 19:40:09 -0600

    adding more projects to web page and styling

--- app/app.js
+++ app/app.js
@@ -15,32 +15,35 @@ var ReactDOM = require('react-dom');
 const Footer = () => {
 	return (
 		<div className="row">
-			<div id="footerText" className="col-xs-12 col-sm-12 col-md-9 col-lg-9">
-        		<p>Copyright 2017 Sarah Diaz-Rivera</p> 
+			<div className="icons col-xs-12 col-sm-12 col-md-3 col-lg-3">
+				<p id="privacy" className="footerText">Privacy Policy</p>
+			</div>
+
+			<div className="footerText col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        		<p id="copyright">Copyright 2017 Sarah Diaz-Rivera</p> 
         	</div>
 
 			<div className="icons col-xs-12 col-sm-12 col-md-3 col-lg-3">
-	        	<img
-	        	id="instagram"
-	            alt="instagram"
-	            src="assets/images/instagram.png"
-	            href="https://www.instagram.com/sarahdiazrivera/"
-	          	/>
-	          	<img
-	          	id="dribbble"
-	            alt="dribbble"
-	            src="assets/images/dribbble.png"
-	          	/>
-	          	<img
-	          	id="linkedin"
-	            alt="linkedin"
-	            src="assets/images/linkedin.png"
-	          	/>
-	          	<img
+	        	<a href="https://github.com/sdr2017" target="_blank"><img
 	          	id="github"
 	            alt="github"
 	            src="assets/images/github.png"
-	          	/>
+	          	/></a>
+	        	<a href="https://www.linkedin.com/in/sarah-diaz-rivera/" target="_blank"><img
+	          	id="linkedin"
+	            alt="linkedin"
+	            src="assets/images/linkedin.png"
+	          	/></a>
+	          	<a href="https://dribbble.com/sdrivera" target="_blank"><img
+	          	id="dribbble"
+	            alt="dribbble"
+	            src="assets/images/dribbble.png"
+	          	/></a>
+	          	<a href="https://www.instagram.com/sarahdiazrivera/" target="_blank"><img
+	        	id="instagram"
+	            alt="instagram"
+	            src="assets/images/instagram.png"
+	          	/></a>
         	</div>        	
 		</div>
 	)
