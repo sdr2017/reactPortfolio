ef1c41955bba7502d40c8a7ba74af503730f0d93
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2018-01-16 21:07:17 -0700

    grabbing id in graphics js

--- public/assets/javascript/graphics.js
+++ public/assets/javascript/graphics.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
 
-window.sr = ScrollReveal();
-sr.reveal('.webProjectDescript', {
-	origin: 'bottom',
-	distance: '20px',
-	rotate: { x: 0, y: 15, z: 0 },
-	duration: 2000,
-	delay: 50,
-	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
-	mobile: true,
-});
+	// Scroll Reveal //
+	window.sr = ScrollReveal();
+	sr.reveal('.webProjectDescript', {
+		origin: 'bottom',
+		distance: '20px',
+		rotate: { x: 0, y: 15, z: 0 },
+		duration: 2000,
+		delay: 50,
+		easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+		mobile: true,
+	});
 
-$('#myTurntable').turntable({
-	axis: 'scroll',
-	scrollStart: 'top',
-	reverse: true,
-});
+	//Turntable.js//
+	$('#myTurntable').turntable({
+		axis: 'scroll',
+		scrollStart: 'top',
+		reverse: true,
+	});
+
+	$(document).on('click', '.projectImage', function() {
+		var value = $(this).attr('id');
+		console.log(value);
+	});
+
 });
